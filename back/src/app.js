bd05f2050e4e38be7a05d839bcbef22ba621fc5c
Purohitdev <devp61285@gmail.com>
2026-02-04 18:05:05 +0530

    Public change to public

--- back/src/app.js
+++ back/src/app.js
@@ -7,7 +7,7 @@ const parth = require('path');
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static("./Public"));
+app.use(express.static("./public"));
 
 
 // create note
